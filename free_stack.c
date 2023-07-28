dceddf529e063525fcaf69b5c6226038ae66dbaf
NanaAba08 <williamsnanaaba@gmail.com>
2023-07-28 03:37:38 -0700

    more betty errors corrected

--- free_stack.c
+++ free_stack.c
@@ -2,13 +2,14 @@
 
 /**
   * free_stack - frees a doubly linked list
-  * *stack: pointer to the head of the stack
+  * @stack: pointer to the head of the stack
   */
 void free_stack(stack_t *stack)
 {
 	while (stack != NULL)
 	{
 		stack_t *temp = stack;
+
 		stack = stack->next;
 		free(temp);
 	}
--- handle_error.c
+++ handle_error.c
@@ -1,22 +1,20 @@
 #include "monty.h"
 
-
- /**
-  * Prints the error message along with the line number and additional info
-   * (if available), and exits the program with EXIT_FAILURE.
-    * @line_number: Line number in the Monty file.
-     * @message: Error message to print.
-      * @stack: Pointer to the top of the stack.
-       * @file: Pointer to the file being processed.
-        */
-        void handle_error(unsigned int line_number, const char *message, stack_t *stack, FILE *file)
-        {
-            fprintf(stderr, "L%d: %s\n", line_number, message);
-                free_stack(stack);
-                    if (file)
-                        {
-                                fclose(file);
-                                    }
-                                        exit(EXIT_FAILURE);
-                                        }
-
+/**
+  * handle_error - Prints the error message along with the line number and
+  * additional info (if available), and exits the program with EXIT_FAILURE.
+  * @line_number: Line number in the Monty file.
+  * @message: Error message to print.
+  * @stack: Pointer to the top of the stack.
+  * @file: Pointer to the file being processed.
+  */
+void handle_error(unsigned int line_number, const char *message, stack_t *stack, FILE *file)
+{
+	fprintf(stderr, "L%d: %s\n", line_number, message);
+	free_stack(stack);
+	if (file)
+	{
+		fclose(file);
+	}
+	exit(EXIT_FAILURE);
+}
